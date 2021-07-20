1e6d1be3e4442706092d94f48ec81cabb7469614
million <milli.gashaw.88@gmail.com>
2021-07-20 12:51:26 +0100

    nltk parssing

--- named_entity_recognition.py
+++ named_entity_recognition.py
@@ -3,5 +3,6 @@ sentence = """At eight o'clock on Thursday morning
 ... Arthur didn't feel very good."""
 tokens = nltk.word_tokenize(sentence)
 tagged = nltk.pos_tag(tokens)
-
 tagged[0:6]
+entities = nltk.chunk.ne_chunk(tagged)
+print(entities)
