02f557c84651e66b65ef5df1aac4758e038b1105
million gashawbeza <milli.gashaw.88@gmail.com>
2021-07-21 12:10:43 +0100

    update

--- flask_route.py
+++ flask_route.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from nltk import word_tokenize
+from markupsafe import escape
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return """<p>Hello, World!</p>
+         <h1>Hello </h1>
+        """
+@app.route("/hello/")
+def hello():
+    return """<p>Hello, World!</p>
+             <h1>Hello </h1>
+            """
+@app.route('/tokenize/<sentence>')
+def w_tokenize(sentence):
+    tokenized_word = str(word_tokenize(sentence))
+    return '<p>Hello, World!</p><h1>Hello </h1> %s ' % escape(tokenized_word)
+
 
